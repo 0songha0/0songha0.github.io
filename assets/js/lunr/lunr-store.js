f5f1b67ffd39efd1f482a744ba61d2049632a7d1
0songha0 <0songha0@users.noreply.github.com>
2025-08-26 07:56:33 +0000

    deploy: 541242538904fcc61ee9e448bfee91097a6803be

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1440,7 +1440,7 @@ var store = [{
         "teaser": null
       },{
         "title": "리눅스 Nginx 서버 구축 및 Vue 프로젝트 빌드 방법",
-        "excerpt":"리눅스 Nginx 설치 시 참고 https://nginx.org/en/linux_packages.html Nginx 공식 홈페이지에서 리눅스 OS 종류별로 설치 방법을 자세히 알려주고 있습니다. 우분투 Nginx 서버 설치 방법 (인터넷O) 인터넷이 가능한 서버에서만 가능한 방법입니다. Nginx 저장소 필수 패키지 설치 sudo apt install curl gnupg ca-certificates lsb-release ubuntu-keyring nginx 저장소 추가 시 필요한 필수 패키지들을 다운로드하여 준비합니다....","categories": ["op"],
+        "excerpt":"리눅스 Nginx 설치 시 참고 https://nginx.org/en/linux_packages.html Nginx 공식 홈페이지에서 리눅스 OS 종류별 Nginx 웹서버 설치 방법을 자세히 알려주고 있습니다. 우분투 Nginx 서버 설치 방법 (인터넷O) 인터넷이 가능한 서버에서만 가능한 방법입니다. Nginx 저장소 필수 패키지 설치 sudo apt install curl gnupg ca-certificates lsb-release ubuntu-keyring nginx 저장소 추가 시 필요한 필수 패키지들을...","categories": ["op"],
         "tags": [],
         "url": "/op/2025-08-21-1",
         "teaser": null
