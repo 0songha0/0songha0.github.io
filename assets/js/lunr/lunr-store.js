a1e214f991a3749a98334db91876872b2829c6f9
0songha0 <0songha0@users.noreply.github.com>
2025-12-04 04:11:02 +0000

    deploy: 4d943f3aa0911fe8ad1d9122c9f23f7597c5b78c

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1512,7 +1512,7 @@ var store = [{
         "teaser": null
       },{
         "title": "윈도우 Claude AI 설치 및 Claude CLI 사용 방법",
-        "excerpt":"윈도우 CLI claude 설치 방법 node 설치 확인 node -v npm -v Window에서 Claude AI를 사용하기 위해서는 node가 설치되어야 합니다. claude 설치 명령어 npm install -g @anthropic-ai/claude-code node가 설치되어 있으면 위 명령어로 claude 패키지를 설치할 수 있습니다. claude 실행 명령어 cd 프로젝트경로 claude claude 화면이 나오면 엔터를 두 번 입력하여...","categories": ["tool"],
+        "excerpt":"윈도우 CLI claude 설치 방법 윈도우 PowerShell 실행 윈도우 검색 &gt; PowerShell 입력 &gt; Windows PowerShell 실행 cmd에서는 질문할 때 여러 줄 불여넣기가 되지 않아 PowerShell로 하는 것이 좋습니다. node 설치 확인 node -v npm -v Window에서 Claude AI를 사용하기 위해서는 node가 설치되어야 합니다. claude 설치 명령어 npm install -g...","categories": ["tool"],
         "tags": [],
         "url": "/tool/2025-11-27-1",
         "teaser": null
